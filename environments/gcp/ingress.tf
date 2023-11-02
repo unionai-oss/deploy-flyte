0809b6d5e76a45217e85dc9b39d4209c111763e0
davidmirror-ops <david espejo@union.ai>
2023-11-02 15:57:50 -0500

    Remove unneeded DNS zone from v1
    Signed-off-by: davidmirror-ops <david espejo@union.ai>

--- environments/gcp/ingress.tf
+++ environments/gcp/ingress.tf
@@ -75,18 +75,3 @@ spec:
     YAML
      )
 }
-resource "google_dns_managed_zone" "flyte-zone" {
-  name        = "flyte-zone"
-  dns_name    = "${local.dns-domain}."
-  description = "DNS Zone created by Terraform to host the FQDN entry for Flyte"
-  
-}
-
-resource "google_dns_record_set" "flyte-host" {
-  name         = "${local.flyte-host}."
-  managed_zone = google_dns_managed_zone.flyte-zone.name
-  type         = "A"
-  ttl          = 300
-  rrdatas      = [module.gke.endpoint]
-  
-}
