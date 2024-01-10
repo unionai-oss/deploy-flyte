3453d56275911652af394b6c9938fe023e016d41
Sarvesh Raj <sarvesh@khiladipro.com>
2024-01-10 20:05:12 +0530

    Update locals.tf

--- environments/gcp/flyte-core/locals.tf
+++ environments/gcp/flyte-core/locals.tf
@@ -3,15 +3,15 @@ locals {
   #Change this on first use
   application            = "flyte" #Change if needed. By default, this is used to build the FQDN for Flyte. See flyte-host notes
   environment            = "gcp" #Change to match your needs
-  project_id             = "<your-GCP-project-ID>"
-  project_number         = "<your-GCP-project-number>"
-  dns-domain             = "<your-domain-name>" #Change to your domain name 
-  region                 = "<your-GCP-region>" #Change to your GCP region
+  project_id             = "kpro-staging"
+  project_number         = "42577133707"
+  dns-domain             = "flyte.khiladipro.com" #Change to your domain name 
+  region                 = "asia-south1" #Change to your GCP region
   
   #You must replace this email address with your own.
   # Let's Encrypt will use this to contact you about expiring
   # certificates, and issues related to your account.
-  email    =             "noreply@flyte.org" 
+  email    =             "shiv@khiladipro.com" 
   
   # Change this only if you need to add more projects in the default installation
   flyte_projects         = ["flytesnacks"]
