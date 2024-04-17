132470d91caa943a5dcdbe46c80a3e83ed89a3e2
davidmirror-ops <david.espejo@union.ai>
2024-04-17 14:06:24 -0500

    Add AKS config for WI federation
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- environments/azure/flyte-core/aks.tf
+++ environments/azure/flyte-core/aks.tf
@@ -1,9 +1,19 @@
+resource "azurerm_user_assigned_identity" "workload_identity" { 
+
+  name = "flyte-workload-identity"
+  resource_group_name = azurerm_resource_group.flyte.name 
+  location = azurerm_resource_group.flyte.location 
+
+}
+
 resource "azurerm_kubernetes_cluster" "flyte" {
-  name                = "${local.tenant}-${local.environment}-flyte"
+  name                = "${local.tenant}-${local.environment}-flytetf"
   location            = azurerm_resource_group.flyte.location
   resource_group_name = azurerm_resource_group.flyte.name
-  dns_prefix          = "${local.tenant}${local.environment}flyte"
-  kubernetes_version  = "1.29.0"
+  dns_prefix          = "${local.tenant}${local.environment}flytetf"
+  workload_identity_enabled = true
+  oidc_issuer_enabled = true
+
 
   default_node_pool {
     name                = "default"
@@ -22,3 +32,26 @@ resource "azurerm_kubernetes_cluster" "flyte" {
     ignore_changes = [default_node_pool]
   }
 }
+
+locals {
+  flyte_ksas = ["default"] #The KSA that Task Pods will use 
+}
+locals {
+  flyte_ksa_ns = toset([
+    for tpl in setproduct(
+      local.flyte_projects,
+      local.flyte_domains,
+      local.flyte_ksas
+    ) : format("%s-%s:%s", tpl...)
+  ])
+}
+resource "azurerm_federated_identity_credential" "federated-identity-creds" {
+  name                = azurerm_user_assigned_identity.workload_identity.name
+  resource_group_name = azurerm_user_assigned_identity.workload_identity.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.workload_identity.id
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.flyte.oidc_issuer_url
+  #subject             = formatlist("system:serviceaccount:[%s]", local.flyte_ksa_ns)
+  subject = "system:serviceaccount:flytesnacks-development:default"
+}
+
