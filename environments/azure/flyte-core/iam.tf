aa9c72abc4a0717868db7240758ed450bba611a8
davidmirror-ops <david.espejo@union.ai>
2024-05-02 14:44:48 -0500

    Fix inexistent Service Principal
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- environments/azure/flyte-core/iam.tf
+++ environments/azure/flyte-core/iam.tf
@@ -13,7 +13,7 @@
 
 #}
 
-resource "azuread_application_registration" "flyte-app" {
+resource "azuread_application_registration" "flyte_app" {
 
 display_name = "flyte-stow-app"
 
@@ -21,8 +21,8 @@ display_name = "flyte-stow-app"
 }
 
 #Service Principal for Flyte tasks
-#resource "azuread_service_principal" "flyte_sp" {
-#  client_id                    = azuread_application_registration.flyte-app.id
+resource "azuread_service_principal" "flyte_stow_sp" {
+  client_id                    = azuread_application_registration.flyte_app.client_id
 #  app_role_assignment_required = false
 #  owners                       = [data.azuread_client_config.current.object_id]
   #app_roles                    = "Storage Blob Data Owner" 
@@ -34,12 +34,12 @@ display_name = "flyte-stow-app"
 #  role_definition_name = each.value
   #principal_id         = data.azurerm_client_config.current.object_id
 #  principal_id         = azuread_service_principal.flyte_sp.object_id
-#}
+}
 
 resource "azurerm_role_assignment" "flyte_sp_role_assignment" {
   scope                = azurerm_storage_account.flyte.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azuread_application_registration.flyte-app.object_id
+  principal_id         = azuread_service_principal.flyte_stow_sp.id
   principal_type = "ServicePrincipal"
 }
 
@@ -49,8 +49,8 @@ resource "time_rotating" "secret_rotation" {
 
 resource "azuread_application_password" "flyte_client_secret" {
  # service_principal_id = azuread_service_principal.flyte_sp.object_id
-  #rotate_when_changed = {
-  #  rotation = time_rotating.secret_rotation.id
-  #}
-  application_id = [azuread_application_registration.flyte-app.id]
+  rotate_when_changed = {
+   rotation = time_rotating.secret_rotation.id
+  }
+  application_id = azuread_application_registration.flyte_app.id
 }
