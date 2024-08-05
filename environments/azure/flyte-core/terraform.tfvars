ebfd93eb14e0d06772c7c2737a5c5c7a7deba3c2
davidmirror-ops <david.espejo@union.ai>
2024-08-05 16:10:39 -0500

    Add vars file
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

---
+++ environments/azure/flyte-core/terraform.tfvars
@@ -0,0 +1,3 @@
+subscription_id = "value"
+tenant_id = "value"
+azure_region = "value"
---
+++ environments/azure/flyte-core/variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id"{ 
+   type = string
+ }
+
+variable "tenant_id"{
+  type = string
+}
+
+#Azure region or zone
+variable "azure_region" {
+type = string
+}
