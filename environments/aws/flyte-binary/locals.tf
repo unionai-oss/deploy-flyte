2744c7f1911fc2e1454db98f7a82b4026328735b
davidmirror-ops <david.espejo@union.ai>
2024-08-09 15:57:40 -0500

    Make use of variables for inputs
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- environments/aws/flyte-binary/locals.tf
+++ environments/aws/flyte-binary/locals.tf
@@ -2,6 +2,15 @@ locals {
   project     = "flyte"
   environment = "terraform" 
   name_prefix = "${local.project}-${local.environment}"
-  #region      = data.aws_region.current.name
   account_id  = data.aws_caller_identity.current.account_id
+  
+  domain_name = "flyte.${data.aws_route53_zone.zone.name}"
+}
+
+data "aws_availability_zones" "available" { state = "available" }
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_route53_zone" "zone" {
+  name = "fthwdemo.run"  # Change this to your Route53 managed zone
 }
--- environments/aws/flyte-binary/terraform.tf
+++ environments/aws/flyte-binary/terraform.tf
@@ -25,13 +25,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    profile =  var.aws_cli_profile #AWS CLI profile name
-    bucket  = var.tfstate_s3_bucket #create an S3 bucket to store Terraform state
-    key     = "terraform.tfstate"
-    region  = var.aws_region #AWS region where the bucket was created
-  }
-
+  
 
 }
 
