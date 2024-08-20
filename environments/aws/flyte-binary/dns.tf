9cd276d27eff76e3660828f80881bc6c91cc3814
davidmirror-ops <david.espejo@union.ai>
2024-08-20 11:46:38 -0500

    Update DNS validation logic
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- environments/aws/flyte-binary/dns.tf
+++ environments/aws/flyte-binary/dns.tf
@@ -8,22 +8,31 @@ resource "aws_acm_certificate" "flyte_cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.flyte_cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.flyte_cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.zone.zone_id}"
-  records = ["${aws_acm_certificate.flyte_cert.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
+resource "aws_route53_record" "cert_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.flyte_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "dns_validated_cert" {
   certificate_arn         = "${aws_acm_certificate.flyte_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
 }
 
 resource "aws_route53_record" "elb_record" {
 
-  provisioner "kubectl-get-ingress" {
+  provisioner "local-exec" {
     command = "export FLYTE_ELB_ADDRESS=$(kubectl get ingress -n flyte -o json | jq -r '.items[].status.loadBalancer.ingress[].ip')"
   }
   zone_id = data.aws_route53_zone.zone.zone_id
