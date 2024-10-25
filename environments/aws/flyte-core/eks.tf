87fa769bbbdc086c55d22f4904cba5f829a2701b
davidmirror-ops <david.espejo@union.ai>
2024-10-25 16:13:05 -0500

    Remove unused fluentbit code
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- environments/aws/flyte-core/eks.tf
+++ environments/aws/flyte-core/eks.tf
@@ -299,46 +299,6 @@ module "aws_cloudwatch_metrics_irsa_role" {
   }
 }
 
-#data "aws_iam_policy_document" "aws_for_fluent_bit_policy" {
- # source_policy_documents = [data.aws_iam_policy.cloudwatch_agent_server_policy.policy]
-
-  # Fluent-bit CloudWatch plugin manages log groups and retention policies
-#  statement {
- #   actions = [
-  #    "logs:DeleteRetentionPolicy",
-   #   "logs:PutRetentionPolicy"
-    #]
-    #resources = ["*"]
-#  }
-#}
-
-#resource "helm_release" "aws_cloudwatch_metrics" {
- # namespace = "kube-system"
- # wait      = true
-  #timeout   = 600
-
-#  name = "aws-cloudwatch-metrics"
-
-#  repository = "https://aws.github.io/eks-charts"
- # chart      = "aws-cloudwatch-metrics"
-  #version    = "0.0.8"
-
-#  set {
- #   name  = "clusterName"
-  #  value = module.eks.cluster_name
-  #}
-
-#  set {
- #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-  #  value = module.aws_cloudwatch_metrics_irsa_role.iam_role_arn
-  #}
-
-#  set {
- #   name  = "tolerations[0].operator"
-  #  value = "Exists"
-  #}
-#}
-
 resource "helm_release" "aws_cloudwatch_observability" {
   namespace = "amazon-cloudwatch"
   create_namespace = true
