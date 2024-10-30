af380a92849fb79534fb1440828ba9b9382ed31c
davidmirror-ops <david.espejo@union.ai>
2024-10-30 16:29:11 -0500

    Intro support for Container Insights
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

---
+++ environments/azure/flyte-core/monitoring.tf
@@ -0,0 +1,71 @@
+locals {
+  flyte_monitored_ns = toset([
+    for tpl in setproduct(
+      local.flyte_projects,
+      local.flyte_domains,
+    ) : format("%s-%s", tpl...)
+  ])
+}
+
+resource "azurerm_log_analytics_workspace" "flyte_logs" {
+  name                = "${local.tenant}-${local.environment}"
+  location            = azurerm_resource_group.flyte.location
+  resource_group_name = azurerm_resource_group.flyte.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_data_collection_rule" "dcr" {
+  name                = "MSCI-${azurerm_log_analytics_workspace.flyte_logs.location}-${azurerm_kubernetes_cluster.flyte.name}"
+  resource_group_name = azurerm_resource_group.flyte.name
+  location            = azurerm_log_analytics_workspace.flyte_logs.location
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.flyte_logs.id
+      name                  = "ciworkspace"
+    }
+  }
+
+  data_flow {
+    streams      = var.streams
+    destinations = ["ciworkspace"]
+  }
+
+  data_flow {
+    streams      = [ "Microsoft-Syslog"]
+    destinations = ["ciworkspace"]
+  }
+
+  data_sources {
+    syslog{
+      streams            = ["Microsoft-Syslog"]
+      facility_names      = var.syslog_facilities
+      log_levels          = var.syslog_levels
+      name               = "sysLogsDataSource"
+    }
+ 
+    extension {
+      streams            = var.streams
+      extension_name     = "ContainerInsights"
+      extension_json     = jsonencode({
+        "dataCollectionSettings" : {
+            "interval": var.data_collection_interval,
+            "namespaceFilteringMode": var.namespace_filtering_mode_for_data_collection,
+            "namespaces": var.namespaces_for_data_collection
+            "enableContainerLogV2": true
+        }
+      })
+      name               = "ContainerInsightsExtension"
+    }
+  }
+
+  description = "DCR for Azure Monitor Container Insights"
+}
+
+resource "azurerm_monitor_data_collection_rule_association" "dcra" {
+  name                        = "ContainerInsightsExtension"
+  target_resource_id          = azurerm_kubernetes_cluster.flyte.id
+  data_collection_rule_id     = azurerm_monitor_data_collection_rule.dcr.id
+  description                 = "Association of container insights data collection rule. Deleting this association will break the data collection for this AKS Cluster."
+}
--- environments/azure/flyte-core/variables.tf
+++ environments/azure/flyte-core/variables.tf
@@ -10,3 +10,30 @@ variable "tenant_id"{
 variable "azure_region" {
 type = string
 }
+
+## Monitoring variables
+variable "streams" {
+ default = ["Microsoft-ContainerLog", "Microsoft-ContainerLogV2", "Microsoft-KubeEvents", "Microsoft-KubePodInventory", "Microsoft-KubeNodeInventory", "Microsoft-KubePVInventory","Microsoft-KubeServices", "Microsoft-KubeMonAgentEvents", "Microsoft-InsightsMetrics", "Microsoft-ContainerInventory",  "Microsoft-ContainerNodeInventory", "Microsoft-Perf"]
+}
+
+variable "syslog_levels" {
+  type = list(string)
+  default = ["Debug", "Info", "Notice", "Warning", "Error", "Critical", "Alert", "Emergency"]
+}
+
+variable "syslog_facilities" {
+  type = list(string)
+  default = ["auth", "authpriv", "cron", "daemon", "mark", "kern", "local0", "local1", "local2", "local3", "local4", "local5", "local6", "local7", "lpr", "mail", "news", "syslog", "user", "uucp"]
+}
+
+variable "data_collection_interval" {
+  default = "1m"
+}
+
+variable "namespace_filtering_mode_for_data_collection" {
+  default = "Off"
+}
+
+variable "namespaces_for_data_collection" {
+  default = ["kube-system", "gatekeeper-system", "flyte", "flytesnacks-development"] #automate this
+}
